0e09ed5fd9687c2af3ea66b89d03adfe28b20dff
Le T <116742517+LeTaih@users.noreply.github.com>
2025-04-10 19:40:49 +0200

    formulaires success

--- src/model/Formulaire.java
+++ src/model/Formulaire.java
@@ -3,5 +3,27 @@ package model;
 public abstract class Formulaire {
 	int jour;
 	int mois;
-	int id;
+	int IdentificationEntite;
+	
+	Formulaire(int jour, int mois) {
+		this.jour = jour;
+		this.mois = mois;
+	}
+
+	public int getJour() {
+		return jour;
+	}
+
+	public int getMois() {
+		return mois;
+	}
+
+	public int getIdentificationEntite() {
+		return IdentificationEntite;
+	}
+
+	public void setIdentificationEntite(int id) {
+		this.IdentificationEntite = id;
+	}
+	
 }
--- src/model/FormulaireHotel.java
+++ src/model/FormulaireHotel.java
@@ -1,18 +1,22 @@
+package model;
+
 public class FormulaireHotel extends Formulaire {
     private int nbLitsSimples;
     private int nbLitsDoubles;
 
-    public FormulaireHotel(int jour, int mois, int numeroChambre, int nbLitsSimples, int nbLitsDoubles) {
-        super(jour, mois, numeroChambre);
+    public FormulaireHotel(int jour, int mois, int nbLitsSimples, int nbLitsDoubles) {
+        super(jour, mois);
         this.nbLitsSimples = nbLitsSimples;
         this.nbLitsDoubles = nbLitsDoubles;
     }
 
-    @Override
-    public void afficher() {
-        System.out.println("Hôtel : " + jour + "/" + mois +
-            " - Chambre " + numeroEntite +
-            " avec " + nbLitsSimples + " lit(s) simple(s) et " +
-            nbLitsDoubles + " lit(s) double(s).");
-    }
+	public int getNbLitsSimples() {
+		return nbLitsSimples;
+	}
+
+	public int getNbLitsDoubles() {
+		return nbLitsDoubles;
+	}
+    
 }
+
